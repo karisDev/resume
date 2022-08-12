30b08b60435f9ae3388cfd4e269878b8813944e8
karisDev <karjaakirill@gmail.com>
2022-08-12 16:02:21 +0300

    update layout and about page

--- src/components/pages/AboutPage.tsx
+++ src/components/pages/AboutPage.tsx
@@ -1,4 +1,5 @@
 import { createSignal } from "solid-js";
+import my_photo from "../../assets/me.png";
 import "../../styles/about.scss";
 
 const AboutPage = ({ russian }: { russian?: boolean | undefined }) => {
@@ -9,8 +10,53 @@ const AboutPage = ({ russian }: { russian?: boolean | undefined }) => {
   return (
     <div class="about_page">
       {!maskHide() && <div class="mask"></div>}
-      <div class=""></div>
-      <h1>Обо мне</h1>
+      <div class="about__content">
+        <div class="about__title">
+          <h1>Обо мне</h1>
+        </div>
+        <div class="about__general">
+          <div class="general_content">
+            <h2>Киреев Кирилл</h2>
+            <h4>Frontend разработчик, 20 лет</h4>
+            <h3>
+              Родился и живу в Москве. Начал заниматься программированием c 6
+              класса. Любовь к автоматизации и созданию красивых и интуитивных
+              интерфейсов.
+            </h3>
+          </div>
+          <img src={my_photo} />
+        </div>
+        <div class="about__education">
+          <h2>Образование</h2>
+          <div class="education_item">
+            <div class="education_date">
+              <h3>2022</h3>
+            </div>
+            <div class="education_content">
+              <h2>Программная инфраструктура</h2>
+              <h2>КРОК</h2>
+            </div>
+          </div>
+          <div class="education_item">
+            <div class="education_date">
+              <h3>3 курс</h3>
+            </div>
+            <div class="education_content">
+              <h2>Информационные Системы и Технологии</h2>
+              <h2>НИТУ МИСиС</h2>
+            </div>
+          </div>
+          <div class="education_item">
+            <div class="education_date">
+              <h3>2019</h3>
+            </div>
+            <div class="education_content">
+              <h2>Агент банка 3 разряда</h2>
+              <h2>Московский колледж бизнес-технологий</h2>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/pages/ResumeBox.tsx
+++ src/components/pages/ResumeBox.tsx
@@ -74,21 +74,21 @@ const ResumeBox: Component = () => {
             onClick={() => beforeNavigate("/resume/ru/about")}
             onMouseEnter={() => mouseOver("fill_about")}
           >
-            <h2>About me</h2>
+            <h2>Обо мне</h2>
           </div>
           <div
             class="projects"
             onClick={() => beforeNavigate("/resume/en/projects")}
             onMouseEnter={() => mouseOver("fill_projects")}
           >
-            <h2>Projects</h2>
+            <h2>Проекты</h2>
           </div>
           <div
             class="contacts"
             onClick={() => beforeNavigate("/resume/en/contacts")}
             onMouseEnter={() => mouseOver("fill_contacts")}
           >
-            <h2>Contacts</h2>
+            <h2>Контакты</h2>
           </div>
         </div>
         {/* used to offset in different origins */}
