858e4685289ea828901adbd90e6a634248980b28
karisDev <karjaakirill@gmail.com>
2022-08-19 23:50:14 +0300

    delete dist from source

---
+++ src/components/UI/InputDefault.tsx
@@ -0,0 +1,8 @@
+import { Component, JSX } from "solid-js";
+
+interface InputDefaultProps {}
+const InputDefault = (props: JSX.ButtonHTMLAttributes<HTMLButtonElement>) => {
+  return <>Hello</>;
+};
+
+export default InputDefault;
