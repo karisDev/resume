d4368b58ee8079d8b67b24ad30f8921bae188cb9
karisDev <karjaakirill@gmail.com>
2023-01-15 14:20:57 +0300

    desktop adjustment

--- src/components/pages/NewAboutPage.tsx
+++ src/components/pages/NewAboutPage.tsx
@@ -3,8 +3,28 @@ import my_photo from "../../assets/about_page/me.png";
 // import "../../assets/about_page/download.svg"; as svg component
 import DownloadSvg from "../../assets/about_page/icons/download.svg";
 import GithubSvg from "../../assets/about_page/icons/github.svg";
+import EducationTitleSvg from "../../assets/about_page/icons/education_title.svg";
+import LanguagesTitleSvg from "../../assets/about_page/icons/languages_title.svg";
+import StackTitleSvg from "../../assets/about_page/icons/stack_title.svg";
 
 const NewAboutPage = ({ russian }: { russian?: boolean | undefined }) => {
+  const educationItems = [
+    {
+      title: "Стажировка",
+      company: "DUC Technologies",
+      year: "2022 - 2023",
+    },
+    {
+      title: "Программная инфраструктура",
+      company: "КРОК",
+      year: "2022",
+    },
+    {
+      title: "Информационные Системы и Технологии",
+      company: "НИТУ МИСиС",
+      year: "2022",
+    },
+  ];
   return (
     <>
       <PagesNav russian={russian} />
@@ -61,6 +81,52 @@ const NewAboutPage = ({ russian }: { russian?: boolean | undefined }) => {
                 красивых, интуитивных интерфейсов. Еще очень люблю английский
                 язык.
               </p>
+            </div>
+          </div>
+        </section>
+        <section class="skills">
+          <div class="grid">
+            <div class="category education">
+              <div class="title">
+                <EducationTitleSvg />
+                <h2>Образование и опыт</h2>
+              </div>
+              {educationItems.map((item) => {
+                return (
+                  <div class="card">
+                    <div class="body">
+                      <h3>{item.title}</h3>
+                      <span>{item.company}</span>
+                    </div>
+                    <span class="horizontal_sep"></span>
+                    <div class="year">
+                      {item.year.includes("-") ? (
+                        <>
+                          {item.year.split("-")[0]}
+                          <br />-<br />
+                          {item.year.split("-")[1]}
+                        </>
+                      ) : (
+                        item.year
+                      )}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+            <span class="separator"></span>
+            <div class="category languages">
+              <div class="title">
+                <LanguagesTitleSvg />
+                <h2>Языки</h2>
+              </div>
+            </div>
+            <span class="separator"></span>
+            <div class="category stack">
+              <div class="title">
+                <StackTitleSvg />
+                <h2>Технологии</h2>
+              </div>
             </div>
           </div>
         </section>
