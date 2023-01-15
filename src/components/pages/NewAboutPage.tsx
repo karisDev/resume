27ad435fad914077407887155e907a75851ae859
karisDev <karjaakirill@gmail.com>
2023-01-15 18:30:39 +0300

    languages

--- src/components/pages/NewAboutPage.tsx
+++ src/components/pages/NewAboutPage.tsx
@@ -6,23 +6,67 @@ import GithubSvg from "../../assets/about_page/icons/github.svg";
 import EducationTitleSvg from "../../assets/about_page/icons/education_title.svg";
 import LanguagesTitleSvg from "../../assets/about_page/icons/languages_title.svg";
 import StackTitleSvg from "../../assets/about_page/icons/stack_title.svg";
+import RussiaSvg from "../../assets/about_page/icons/russia.svg";
+import UsSvg from "../../assets/about_page/icons/us.svg";
+import LanguageCsharp from "../../assets/about_page/icons/csharp.svg";
+import LanguageCSS from "../../assets/about_page/icons/css.svg";
+import LanguageHTML from "../../assets/about_page/icons/html.svg";
+import LanguageJS from "../../assets/about_page/icons/js.svg";
+import LanguagePython from "../../assets/about_page/icons/python.svg";
+import LanguageTS from "../../assets/about_page/icons/ts.svg";
+import LanguageDart from "../../assets/about_page/icons/dart.svg";
 
 const NewAboutPage = ({ russian }: { russian?: boolean | undefined }) => {
   const educationItems = [
     {
-      title: "Стажировка",
+      title: "Стажровка",
       company: "DUC Technologies",
-      year: "2022 - 2023",
+      year: "2022\n2023",
     },
     {
-      title: "Программная инфраструктура",
+      title: "Программная\nинфраструктура",
       company: "КРОК",
       year: "2022",
     },
     {
-      title: "Информационные Системы и Технологии",
+      title: "Информационные\nСистемы и Технологии",
       company: "НИТУ МИСиС",
-      year: "2022",
+      year: "2020\n2024",
+    },
+    {
+      title: "Агент банка 3 разряда",
+      company: "Московский колледж\nбизнес технологий",
+      year: "2019",
+    },
+  ];
+  const programmingItems = [
+    {
+      title: "TypeScript",
+      icon: LanguageTS,
+    },
+    {
+      title: "JavaScript",
+      icon: LanguageJS,
+    },
+    {
+      title: "Python",
+      icon: LanguagePython,
+    },
+    {
+      title: "HTML",
+      icon: LanguageHTML,
+    },
+    {
+      title: "CSS",
+      icon: LanguageCSS,
+    },
+    {
+      title: "C#",
+      icon: LanguageCsharp,
+    },
+    {
+      title: "Dart",
+      icon: LanguageDart,
     },
   ];
   return (
@@ -75,12 +119,12 @@ const NewAboutPage = ({ russian }: { russian?: boolean | undefined }) => {
           <div class="content">
             <div class="body">
               <h1>Фронтенд разработчик, 20 лет</h1>
-              <p>
+              <h3>
                 Родился и живу в Москве. Начал заниматься программированием c 5
                 класса на Quick Basic. Любовь к автоматизации и созданию
                 красивых, интуитивных интерфейсов. Еще очень люблю английский
                 язык.
-              </p>
+              </h3>
             </div>
           </div>
         </section>
@@ -91,37 +135,57 @@ const NewAboutPage = ({ russian }: { russian?: boolean | undefined }) => {
                 <EducationTitleSvg />
                 <h2>Образование и опыт</h2>
               </div>
-              {educationItems.map((item) => {
-                return (
-                  <div class="card">
-                    <div class="body">
-                      <h3>{item.title}</h3>
-                      <span>{item.company}</span>
+              <div class="cards">
+                {educationItems.map((item) => {
+                  return (
+                    <div class="card">
+                      <div class="body">
+                        <p>{item.title}</p>
+                        <span>{item.company}</span>
+                      </div>
+                      <span class="horizontal_sep"></span>
+                      <div class="year">{item.year}</div>
                     </div>
-                    <span class="horizontal_sep"></span>
-                    <div class="year">
-                      {item.year.includes("-") ? (
-                        <>
-                          {item.year.split("-")[0]}
-                          <br />-<br />
-                          {item.year.split("-")[1]}
-                        </>
-                      ) : (
-                        item.year
-                      )}
-                    </div>
-                  </div>
-                );
-              })}
+                  );
+                })}
+              </div>
             </div>
-            <span class="separator"></span>
+            <span class="separator sep1"></span>
             <div class="category languages">
               <div class="title">
                 <LanguagesTitleSvg />
                 <h2>Языки</h2>
               </div>
+              <div class="languages__group">
+                <h3 class="languages_title">Естественные</h3>
+                <div class="language">
+                  <RussiaSvg />
+                  <p>Русский</p>
+                  <span class="separator"></span>
+                  <p>Родной</p>
+                </div>
+                <div class="language">
+                  <UsSvg />
+                  <p>Английский</p>
+                  <span class="separator"></span>
+                  <p>C1</p>
+                </div>
+              </div>
+              <div class="languages__group">
+                <h3 class="languages_title">Программирования</h3>
+                <div class="programming">
+                  {programmingItems.map((item) => {
+                    return (
+                      <div class="programming__item">
+                        <item.icon />
+                        <p>{item.title}</p>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
             </div>
-            <span class="separator"></span>
+            <span class="separator sep2"></span>
             <div class="category stack">
               <div class="title">
                 <StackTitleSvg />
