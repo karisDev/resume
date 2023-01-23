5638e55468a7ccb9d87295e00eaefbbefbd0600a
karisDev <karjaakirill@gmail.com>
2023-01-23 17:17:17 +0300

    add projects and description

--- src/components/pages/NewProjectsPage.tsx
+++ src/components/pages/NewProjectsPage.tsx
@@ -1,11 +1,17 @@
 import PagesNav from "../ui/PagesNav";
 import ChevronRightSvg from "../../assets/icons/chevron_right.svg";
-import project_tb1 from "../../assets/images/tb1.png";
-import project_tb2 from "../../assets/images/tb2.png";
-import project_tb3 from "../../assets/images/tb3.png";
 import FirebaseSvg from "../../assets/icons/firebase.svg";
 import MuiSvg from "../../assets/icons/mui.svg";
 import TypescriptSvg from "../../assets/icons/ts.svg";
+import SeleniumSvg from "../../assets/icons/selenium.svg";
+import CsharpSvg from "../../assets/icons/csharp.svg";
+import PythonSvg from "../../assets/icons/python.svg";
+import ReactSvg from "../../assets/icons/reactjs.svg";
+import JavaScriptSvg from "../../assets/icons/js.svg";
+import SassSvg from "../../assets/icons/sass.svg";
+import project_tb1 from "../../assets/images/tb1.png";
+import project_tb2 from "../../assets/images/tb2.png";
+import project_tb3 from "../../assets/images/tb3.png";
 import project_tb4 from "../../assets/images/tb4.png";
 import project_cs1 from "../../assets/images/cambridge1.png";
 import project_cs2 from "../../assets/images/cambridge2.png";
@@ -14,12 +20,13 @@ import project_aidoc1 from "../../assets/images/aidoc1.png";
 import project_aidoc2 from "../../assets/images/aidoc2.png";
 import project_aidoc3 from "../../assets/images/aidoc3.png";
 import project_aidoc4 from "../../assets/images/aidoc4.png";
-import SeleniumSvg from "../../assets/icons/selenium.svg";
-import CsharpSvg from "../../assets/icons/csharp.svg";
-import PythonSvg from "../../assets/icons/python.svg";
-import ReactSvg from "../../assets/icons/reactjs.svg";
-import JavaScriptSvg from "../../assets/icons/js.svg";
-import SassSvg from "../../assets/icons/sass.svg";
+import project_lct1 from "../../assets/images/lct1.png";
+import project_lct2 from "../../assets/images/lct2.png";
+import project_lct3 from "../../assets/images/lct3.png";
+import project_lct4 from "../../assets/images/lct4.png";
+import project_directions1 from "../../assets/images/directions1.png";
+import project_directions2 from "../../assets/images/directions2.png";
+import project_directions3 from "../../assets/images/directions3.png";
 
 import { createSignal, For } from "solid-js";
 import { Transition } from "solid-transition-group";
@@ -50,14 +57,14 @@ const projects: Project[] = [
     name: "AiDoc",
     shortDescription: "Определение вида договора с интерпретацией результатов",
     fullDescription:
-      "Кейс №1 в хакатоне Xmas Hack. 4 место. Задача была понять тип документа, поступающего на вход. Была проделана крупная работа вместе с дизайнером и бэкэнд разработчиком. Интеграция бэкэнд была сделана за 1 день.",
+      "Кейс №1 в хакатоне Xmas Hack 2022. 4 место. Задача была понять тип документа с помощью нейросетей. Была тесная работа вместе с дизайнером и бэкэнд разработчиком. Участие в создании прототипа.",
     backgroundColor: "#1c8dd9",
     themeColor: "#fff",
     images: [project_aidoc1, project_aidoc2, project_aidoc3, project_aidoc4],
     info: [
       {
         title: "Фронт",
-        description: "React\nJavaScipt",
+        description: "React",
       },
       {
         title: "Библиотеки",
@@ -65,7 +72,8 @@ const projects: Project[] = [
       },
       {
         title: "Особенности",
-        description: "Командная работа\nPixel Perfect\nАнимации",
+        description:
+          "Загрузка с анимацией\nКомандная работа\nPixel Perfect\nАвторизация",
       },
     ],
     stackIcons: [ReactSvg, JavaScriptSvg, SassSvg],
@@ -73,7 +81,11 @@ const projects: Project[] = [
     links: [
       {
         title: "Демо (без бэкэнда)",
-        url: "https://karisdev.github.io/",
+        url: "https://denmalbas007.github.io/AiDoc/",
+      },
+      {
+        title: "Репозиторий",
+        url: "https://github.com/denmalbas007/AiDoc/",
       },
     ],
   },
@@ -81,7 +93,7 @@ const projects: Project[] = [
     name: "Task board",
     shortDescription: "Органайзер с большим стеком. Один из первых проектов",
     fullDescription:
-      "Веб сайт на React для продуктивности. Один из первых проектов. Целью было изучить как можно больше практик. Был сделан собственный дизайн в Figma на основе доски Kanban",
+      "Веб сайт на React для продуктивности. Один из первых проектов. Целью было получить как можно больше знаний на старте. Был сделан собственный дизайн в Figma на основе доски Kanban",
     backgroundColor: "#f1f1f1",
     images: [project_tb1, project_tb2, project_tb3, project_tb4],
     info: [
@@ -101,19 +113,28 @@ const projects: Project[] = [
     ],
     stackIcons: [FirebaseSvg, MuiSvg, TypescriptSvg],
     isDarkTheme: false,
+    links: [
+      {
+        title: "Демо",
+        url: "https://karisdev.github.io/react-task-board/",
+      },
+      {
+        title: "Репозиторий",
+        url: "https://github.com/karisDev/react-task-board/",
+      },
+    ],
   },
   {
     name: "Cambridge Solver",
-    shortDescription:
-      "Приложение автоматизации сайта Cambridge для парсинга ответов",
+    shortDescription: "Автоматизация сайта Cambridge для парсинга ответов",
     fullDescription:
-      "Клиент-серверный проект, в основу которого входит автоматизация браузера с целью ввода правильных ответов на сайте. Приложение, которое знают 99% студентов мисис 24 и 23 года выпуска.",
+      "Клиент-серверный проект, в основу которого входит автоматизация браузера с целью ввода правильных ответов на сайте Cambridge One. Знают 99% студентов мисис 2023 и 2024 года выпуска. Позже было сделано расширение. Платформа перестала работать в России",
     backgroundColor: "#303030",
     images: [project_cs1, project_cs2, project_cs3],
     info: [
       {
         title: "Клиент",
-        description: "Selenium\nC# WPF\nChrome",
+        description: "Selenium\nC# WPF\nChrome\nMaterial Design",
       },
       {
         title: "Сервер",
@@ -125,9 +146,75 @@ const projects: Project[] = [
           "120 000 решенных тестов\n900+ пользователей\n200+ человек в группе",
       },
     ],
+    links: [
+      {
+        title: "Репозиторий",
+        url: "https://github.com/karisDev/CambridgeOneSolver/",
+      },
+      {
+        title: "VK",
+        url: "https://vk.com/cambridgeonesolver",
+      },
+      {
+        title: "Telegram",
+        url: "https://t.me/cambridgeonesolver",
+      },
+    ],
     themeColor: "#FFC107",
     stackIcons: [CsharpSvg, PythonSvg, SeleniumSvg],
     isDarkTheme: true,
+  },
+  {
+    name: "ЛЦТ 2022",
+    shortDescription: "Анализ перспективных ниш для инвестирования",
+    fullDescription:
+      "Кейс №4 хакатона Лидеры Цифровой Трансформации 2022. Единственная молодая команда прошедшая в финал. Целью было найти перспективные отрасли для импортозамещения. 12 API запросов были интегрированы за один день. Первый опыт создания полного дизайна приложения.\nВход для демо: user/user. Возможно уже не работает.",
+    backgroundColor: "#9b1827",
+    images: [project_lct1, project_lct2, project_lct3, project_lct4],
+    info: [
+      {
+        title: "Фронт",
+        description: "React\nTypescript\nSCSS\nAxios",
+      },
+      {
+        title: "Особенности",
+        description:
+          "Командная работа\nСобственный дизайн\nАдаптивность\nАвторизация",
+      },
+    ],
+    links: [
+      {
+        title: "Репозиторий",
+        url: "https://github.com/denmalbas007/RecSys_LCT",
+      },
+      {
+        title: "Демо (может не работать)",
+        url: "http://37.230.196.148:2002/",
+      },
+    ],
+    isDarkTheme: true,
+    stackIcons: [ReactSvg, TypescriptSvg, SassSvg],
+  },
+  {
+    name: "Directions",
+    shortDescription: "Запись трафика между улицами или городами",
+    fullDescription:
+      "Приложение для сбора статистики в Excel о трафике между 2-10 разными точками на карте, работает с помощью Google Directions API. Понадобилось для автоматизации действий в курсовой работе. Умеет работать в фоне, чтобы собирать данные по таймерам. Красивый дизайн :)",
+    backgroundColor: "#3f51b5",
+    isDarkTheme: true,
+    images: [project_directions1, project_directions2, project_directions3],
+    info: [
+      {
+        title: "Стек",
+        description: "C# WPF\nDirections API\nMaterial Design\nBallonTip",
+      },
+    ],
+    links: [
+      {
+        title: "Репозиторий",
+        url: "https://github.com/karisDev/DirectionsInformation/",
+      },
+    ],
   },
 ];
 
